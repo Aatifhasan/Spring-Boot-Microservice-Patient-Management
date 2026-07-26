f58a9af93388c6123f4f5952528ae4d66b52825f
Aatif <Aatif@gmail.com>
2026-07-26 16:26:42 +0530

    Added gRPC integration

--- patient_service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient_service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -4,6 +4,7 @@ import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.exception.PatientNotFoundException;
+import com.pm.patient_service.grpc.BillingServiceGrpcClient;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.models.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -16,10 +17,13 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
+
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient= billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getAllPatients() {
@@ -38,6 +42,9 @@ public class PatientService {
         }
 
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        // Call the Billing Service to create a billing account for the new patient
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
         return PatientMapper.toDTO(newPatient);
     }
 
